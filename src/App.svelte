33587fb793e69ee6afb1c974971f37a839d640db
Airbus5717 <Abdussamadf350@gmail.com>
2021-06-26 17:19:53 +0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Menu from "./Menu.svelte";
+</script>
+
+<main>
+	<h1>hmm</h1>
+	<Menu />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff2a00;
+		text-transform: uppercase;
+		font-size: 2em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+		h1 {
+			font-size: xx-large;
+		}
+	}
+</style>
---
+++ src/Menu.svelte
@@ -0,0 +1,15 @@
+<script>
+    import AudioPlayer, { stopAll } from "./AudioPlayer.svelte";
+    let audioTracks = ["./audio/1.mp3", "./audio/2.mp3"];
+</script>
+
+<main>
+    <button on:click={stopAll}> Stop all! </button>
+
+    {#each audioTracks as src}
+        <AudioPlayer {src} />
+    {/each}
+</main>
+
+<style>
+</style>
